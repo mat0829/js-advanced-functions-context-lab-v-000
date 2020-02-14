c4c3e767766eb183241bea6d12b5d87fe7b2ce3a
Mat Anderson <matandersonlv@hotmail.com>
2020-02-14 20:09:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* Your Code Here */
 
+let 
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
